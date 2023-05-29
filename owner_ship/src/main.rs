59ad92d151eecbb42f7f4b15490fc56aa285cb3b
salimp2009@gmail.com <salimp2009@gmail.com>
2023-05-29 14:54:55 +0300

    continue on RcM; reference counting single thread shared pointer

--- owner_ship/src/main.rs
+++ owner_ship/src/main.rs
@@ -14,6 +14,15 @@ pub fn gives_ownership() -> String {
     String::from("owner is yours")
 }
 
+pub fn takes_and_gives_ownership(a_string: String) -> String {
+    a_string
+}
+
+pub fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
+}
+
 fn main() {
     let s1 = String::from("hello");
     let s2 = s1.clone();
@@ -30,4 +39,9 @@ fn main() {
 
     let s3 = gives_ownership();
     println!("gives_ownership: {s3}");
+    let s4 = takes_and_gives_ownership(s3);
+    // println!("s3 is moved: {s3}");
+    println!("takes_and_gives_ownership: {s4}");
+    let (s5, len) = calculate_length(s4);
+    println!("string: {s5} and length: {len}");
 }
--- pointers_cell/src/rc_m.rs
+++ pointers_cell/src/rc_m.rs
@@ -1,27 +1,52 @@
-use std::ops::Deref;
+use crate::cel_m::CellM;
 
 struct RcInner<T> {
     value: T,
-    refcount: usize,
+    refcount: CellM<usize>,
 }
 
 pub struct RcM<T> {
     inner: *const RcInner<T>,
 }
 
+impl<T> RcM<T> {
+    pub fn new(v: T) -> Self {
+        let inner = Box::new(RcInner {
+            value: v,
+            refcount: CellM::new(1),
+        });
+        RcM {
+            inner: Box::into_raw(inner),
+        }
+    }
+}
+
 // impl<T> Copy for RcM<T> {}
 
-// impl<T> Clone for RcM<T> {
-//     fn clone(&self) -> Self {
-//         let inner = unsafe { &*self.inner };
-//         inner.refcount += 1;
-//         RcM { inner: self.inner }
-//     }
-// }
+impl<T> Clone for RcM<T> {
+    fn clone(&self) -> Self {
+        let inner = unsafe { &*self.inner };
+        let count = inner.refcount.get();
+        inner.refcount.set(count + 1);
+        RcM { inner: self.inner }
+    }
+}
 
 impl<T> std::ops::Deref for RcM<T> {
     type Target = T;
     fn deref(&self) -> &Self::Target {
+        // SAFETY: self.inner is a Box that is allocated
+        //  when the last reference goes away
         &unsafe { &*self.inner }.value
+    }
+}
+
+impl<T> Drop for RcM<T> {
+    fn drop(&mut self) {
+        let inner = unsafe { &*self.inner };
+        let rc = inner.refcount.get();
+        if rc == 1 {
+            //SAFETY: we are the
+        }
     }
 }
