cdffe9cf9d8aa65a062dcf52685cd97171fba1c7
salimp2009@gmail.com <salimp2009@gmail.com>
2023-05-29 18:33:06 +0300

    continued to work on our implementation of RefCell aka Rc pointer; shared referenced pointer

--- pointers_cell/src/rc_m.rs
+++ pointers_cell/src/rc_m.rs
@@ -1,3 +1,6 @@
+#![allow(clippy::drop_ref)]
+use std::ptr::NonNull;
+
 use crate::cel_m::CellM;
 
 struct RcInner<T> {
@@ -6,7 +9,7 @@ struct RcInner<T> {
 }
 
 pub struct RcM<T> {
-    inner: *const RcInner<T>,
+    inner: NonNull<RcInner<T>>,
 }
 
 impl<T> RcM<T> {
@@ -16,19 +19,8 @@ impl<T> RcM<T> {
             refcount: CellM::new(1),
         });
         RcM {
-            inner: Box::into_raw(inner),
+            inner: unsafe { NonNull::new_unchecked(Box::into_raw(inner)) },
         }
-    }
-}
-
-// impl<T> Copy for RcM<T> {}
-
-impl<T> Clone for RcM<T> {
-    fn clone(&self) -> Self {
-        let inner = unsafe { &*self.inner };
-        let count = inner.refcount.get();
-        inner.refcount.set(count + 1);
-        RcM { inner: self.inner }
     }
 }
 
@@ -37,16 +29,31 @@ impl<T> std::ops::Deref for RcM<T> {
     fn deref(&self) -> &Self::Target {
         // SAFETY: self.inner is a Box that is allocated
         //  when the last reference goes away
-        &unsafe { &*self.inner }.value
+        &unsafe { self.inner.as_ref() }.value
+    }
+}
+
+impl<T> Clone for RcM<T> {
+    fn clone(&self) -> Self {
+        let inner = unsafe { self.inner.as_ref() };
+        let count = inner.refcount.get();
+        inner.refcount.set(count + 1);
+        RcM { inner: self.inner }
     }
 }
 
 impl<T> Drop for RcM<T> {
     fn drop(&mut self) {
-        let inner = unsafe { &*self.inner };
+        let inner = unsafe { self.inner.as_ref() };
         let rc = inner.refcount.get();
         if rc == 1 {
-            //SAFETY: we are the
+            drop(inner);
+            //SAFETY: we are the only Rc left and we are being dropped
+            //therefore there can not be any Rc
+            let _ = unsafe { Box::from_raw(self.inner.as_ptr()) };
+        } else {
+            // there are other Rcs don't drop the box
+            inner.refcount.set(rc - 1);
         }
     }
 }
