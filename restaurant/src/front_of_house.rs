c4dc1a1695ec39b90fa28a0ef01bbd6fd8df85e4
salimp2009@gmail.com <salimp2009@gmail.com>
2023-06-08 17:34:52 +0300

    added basic example of visibility with pub keyword

--- restaurant/src/front_of_house.rs
+++ restaurant/src/front_of_house.rs
@@ -21,20 +21,54 @@ pub mod serving {
 }
 
 pub fn eat_at_restaurant() {
-    // absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
-    // relative path
-    hosting::add_to_waitlist();
-    serving::take_order();
+    let mut meal = back_of_house::Breakfast::summer_toast("Rye");
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    meal.toast = String::from("Wheat");
+    println!("that is a great {} toast!", meal.toast);
+    println!("Orders: {order1:?}, {order2:?}");
 }
 
 fn deliver_order() {}
 
 mod back_of_house {
+    // all fields in enum is public
+    #[derive(Debug)]
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         super::deliver_order();
     }
 
     fn cook_order() {}
+
+    #[derive(Debug)]
+    pub struct Breakfast {
+        pub toast: String,
+        // if not identified with pub this field is private
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer_toast(toast: &str) -> Self {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("apple"),
+            }
+        }
+    }
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_restaurant() {
+        eat_at_restaurant();
+    }
 }
