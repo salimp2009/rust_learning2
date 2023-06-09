277be6a2ff85c18b25afea8b72605a7e3ae9f8bc
salimp2009@gmail.com <salimp2009@gmail.com>
2023-06-09 14:30:27 +0300

    started examples of Fn, FnMut, FnOnce

--- funct_clojures/src/main.rs
+++ funct_clojures/src/main.rs
@@ -1,10 +1,10 @@
 use std::mem::{size_of, size_of_val};
 
-pub fn bar<T>() -> usize {
-    size_of::<T>()
+pub fn bar<T>() {
+    size_of::<T>();
 }
 
-pub fn baz(f: fn() -> usize) {
+pub fn baz(f: fn()) {
     // add code here
     println!("size of f: {}", size_of_val(&f));
 }
@@ -13,14 +13,24 @@ pub fn foo() -> usize {
     5
 }
 
+pub fn quox<F>(f: F)
+where
+    F: Fn(),
+{
+}
+
 fn main() {
     println!("Hello, world!");
     // type of x is function item
     let x = bar::<usize>;
     // x = bar::<isize>;
+    // function items are zero sized
     println!("size of funct item: {}", std::mem::size_of_val(&x));
     //function item coerces to fn pointer
+    // and since funct item is coerced into func pointer
+    // size of it becomes size of a pointer (8 bytes here)
     baz(x);
     baz(bar::<usize>);
     baz(bar::<isize>);
+    quox(bar::<usize>);
 }
