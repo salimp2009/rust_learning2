2bf835f5dd59e9ef132121b66a41417ddddc5585
salimp2009@gmail.com <salimp2009@gmail.com>
2023-06-15 15:16:14 +0300

    started implementing Drop for custom Boks (as Box)

--- drop_check/src/main.rs
+++ drop_check/src/main.rs
@@ -1,6 +1,16 @@
 #![allow(dead_code)]
+
+#[derive(Debug)]
 pub struct Boks<T> {
     p: *mut T,
+}
+
+impl<T> Drop for Boks<T> {
+    fn drop(&mut self) {
+        unsafe { Box::from_raw(self.p) };
+        // this will drop the T but not free the Box
+        // unsafe { std::ptr::drop_in_place(self.p) }
+    }
 }
 
 impl<T> Boks<T> {
@@ -11,4 +21,13 @@ impl<T> Boks<T> {
     }
 }
 
-fn main() {}
+fn main() {
+    let x = 42;
+
+    {
+        let y = Boks::new(x);
+        assert_eq!(unsafe { *(y.p) }, x);
+        println!("Boks: {:#?}", unsafe { *(y.p) });
+    }
+    println!("x: {x}");
+}
