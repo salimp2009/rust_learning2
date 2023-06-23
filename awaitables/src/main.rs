437e57d857376a793de996c7b1efba275ff7a28b
salimp2009@gmail.com <salimp2009@gmail.com>
2023-06-23 15:18:38 +0300

    started basic examples for async/await

--- awaitables/src/main.rs
+++ awaitables/src/main.rs
@@ -1,6 +1,26 @@
-#![allow(dead_code)]
-pub async fn foo1() {}
+use std::future::Future;
+
+// #![allow(dead_code)]
+pub async fn foo1() -> usize {
+    println!("foo1");
+    foo2().await;
+    0
+}
+
+pub fn foo2() -> impl Future<Output = usize> {
+    async {
+        println!("foo2");
+        0
+    }
+}
+
+pub async fn foo3() -> usize {
+    let x = foo1().await;
+    x
+}
 
 pub fn main() {
-    println!("Hello, world!");
+    println!("hello from main");
+    // let x = foo3();
+    // println!("x: {:#?}", x);
 }
