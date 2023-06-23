a5d5e5137af3fd7c10ece1f6c2666a16ef46dc75
salimp2009@gmail.com <salimp2009@gmail.com>
2023-06-23 18:56:35 +0300

    added future create to be able to use futures

--- awaitables/src/main.rs
+++ awaitables/src/main.rs
@@ -1,26 +1,42 @@
 use std::future::Future;
 
+use futures::executor::block_on;
+
 // #![allow(dead_code)]
 pub async fn foo1() -> usize {
-    println!("foo1");
-    foo2().await;
+    println!("foo1 is called");
     0
 }
 
+#[allow(clippy::manual_async_fn)]
 pub fn foo2() -> impl Future<Output = usize> {
     async {
+        println!("printing foo1 in foo2 soon");
+        foo1().await;
         println!("foo2");
         0
     }
 }
 
 pub async fn foo3() -> usize {
-    let x = foo1().await;
-    x
+    foo1().await
+}
+
+async fn hello_world() {
+    println!("hello async");
 }
 
 pub fn main() {
     println!("hello from main");
-    // let x = foo3();
-    // println!("x: {:#?}", x);
+    let _y = foo2();
+    let _z = async {
+        let x = foo3().await;
+        println!("x: {:#?}", x);
+        x
+    };
+    let future = hello_world();
+    block_on(future);
+    block_on(foo1());
+    block_on(foo2());
+    block_on(foo3());
 }
