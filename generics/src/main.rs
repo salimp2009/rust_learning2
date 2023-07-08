3a5b713fb04063daa980daa2db09bd100e977b21
salimp2009@gmail.com <salimp2009@gmail.com>
2023-07-08 15:24:57 +0300

    continued with the generics

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,5 +1,5 @@
 use std::fmt::Debug;
-pub fn largest_num<T>(list: &[T]) -> T
+pub fn largest_any<T>(list: &[T]) -> &T
 where
     T: Copy + Debug + PartialOrd + PartialEq,
 {
@@ -10,10 +10,21 @@ where
         }
     });
     println!("collection received: {:#?}", largest);
-    *largest
+    largest
 }
+
+#[derive(Debug)]
+pub struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
 fn main() {
     let my_vec = vec![1, 2, 3, 4];
-    largest_num(&[1, 2, 3, 4]);
-    largest_num(&my_vec);
+    largest_any::<i32>(&[1, 2, 3, 4]);
+    largest_any(&my_vec);
+    let char_vec = vec!["a", "b", "c"];
+    largest_any(&char_vec);
+    let point1 = Point { x: 5, y: 15.6 };
+    println!("point1: {point1:?},\nx: {}, y:{}", point1.x, point1.y);
 }
