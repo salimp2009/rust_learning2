b0d76bc7fef4c1396c4f3a3c48a238781e5d1752
salimp2009@gmail.com <salimp2009@gmail.com>
2023-07-10 15:58:19 +0300

    added types and impl Summarize for those types as a basic example

---
+++ trait_interface/src/lib.rs
@@ -0,0 +1,47 @@
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+#[derive(Debug)]
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+#[derive(Debug)]
+pub struct Twitter {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Twitter {
+    fn summarize(&self) -> String {
+        format!("user '{}' says '{}' ", self.username, self.content)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let twitter1 = Twitter {
+            username: "salitos".to_string(),
+            content: "heyyo friends".to_string(),
+            reply: true,
+            retweet: false,
+        };
+        println!("twitter1: {}", twitter1.summarize());
+    }
+}
