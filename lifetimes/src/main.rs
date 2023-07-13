9615523e42e5e7590a7451694b37deb84dc29158
salimp2009@gmail.com <salimp2009@gmail.com>
2023-07-14 00:24:44 +0300

    continue on lifetimes; not much done

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -12,7 +12,16 @@ pub fn lifetimes_functions1() {
     let result = longest(string1.as_str(), string2);
     println!("longest string is {result}");
 }
+pub fn lifetimes_functions2() {
+    let string1 = String::from("looser");
+    {
+        let string2 = "winner";
+        let result = longest(string1.as_str(), string2);
+        println!("longest string is {result}");
+    }
+}
 
 fn main() {
     lifetimes_functions1();
+    lifetimes_functions2();
 }
