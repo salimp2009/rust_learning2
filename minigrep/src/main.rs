045a7e5e64b640a23e4e5ccdd0ed5c7c72920211
salimp2009@gmail.com <salimp2009@gmail.com>
2023-07-22 16:24:25 +0300

    started minigrep commandline app

---
+++ minigrep/src/main.rs
@@ -0,0 +1,12 @@
+use std::{env, fs};
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("Searching for '{query}' ");
+    println!("In file '{file_path}' ");
+
+    let contents = fs::read_to_string(file_path)
+        .expect("File Read Error: either file does not exist or incorrect path!");
+    println!("With {query}:\n{contents}")
+}
