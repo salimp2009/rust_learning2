0a8e789e4f0f5b6e65eca29aefbd38afdafc559f
salimp2009@gmail.com <salimp2009@gmail.com>
2023-07-27 22:29:53 +0300

    seperated the Config and run function from main.rs to lib.rs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,35 +1,6 @@
-use std::{env, error::Error, fs, process};
+use std::{env, process};
 
-pub fn parse_config(args: &[String]) -> Config {
-    let query = &args[1].as_str();
-    let file_path = &args[2].as_str();
-    Config { query, file_path }
-}
-
-// #[derive(Debug, Clone, Copy)]
-pub struct Config<'a> {
-    pub query: &'a str,
-    pub file_path: &'a str,
-}
-
-// TODO: check if it is OK to use &str here
-impl<'a> Config<'a> {
-    pub fn build(args: &'a [String]) -> Result<Config<'a>, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments!");
-        }
-
-        let query = args[1].as_str();
-        let file_path = args[2].as_str();
-        Ok(Config { query, file_path })
-    }
-}
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With {}:\n{}", config.query, contents);
-    Ok(())
-}
+use minigrep::Config;
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
@@ -38,7 +9,7 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error : {e}");
         process::exit(1);
     };
