eb790f3c49f2e526d71346bebf9676faaa3637cf
salimp2009@gmail.com <salimp2009@gmail.com>
2023-08-06 23:22:08 +0300

    added inventory api to main and it works

--- tshirt_co/src/main.rs
+++ tshirt_co/src/main.rs
@@ -2,6 +2,18 @@ use tshirt_co::inventory::{self, ShirtColor};
 
 fn main() {
     let store = inventory::Inventory {
-        shirts: vec![ShirtColor::Red, ShirtColor::Blue],
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
     };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1.unwrap(),
+        giveaway
+    );
+
+    let user_pref2 = None;
+    let giveaway = store.giveaway(user_pref2);
+    println!("User with no preference gets {giveaway:?}");
 }
