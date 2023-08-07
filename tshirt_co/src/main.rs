c1d34d6e5e57bb0d798b74c5a6aefa0ccdc6cd90
salimp2009@gmail.com <salimp2009@gmail.com>
2023-08-07 23:31:50 +0300

    doing more exampes for closures

--- tshirt_co/src/main.rs
+++ tshirt_co/src/main.rs
@@ -1,3 +1,5 @@
+use std::{thread, time::Duration};
+
 use tshirt_co::inventory::{self, ShirtColor};
 
 fn main() {
@@ -16,4 +18,16 @@ fn main() {
     let user_pref2 = None;
     let giveaway = store.giveaway(user_pref2);
     println!("User with no preference gets {giveaway:?}");
+
+    // adding type annotation for closures is optional
+    let _expensive_closure = |num: u32| -> u32 {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
+    let example_closure = |x| x;
+    let s = example_closure("hello".to_string());
+    // this wont work since the type for input in closure is set to String in above code
+    // let n = example_closure(5);
 }
