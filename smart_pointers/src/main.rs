75b8ac4ef6b629e9a791301b4d82414a31904049
salimp2009@gmail.com <salimp2009@gmail.com>
2023-08-18 00:35:01 +0300

    added example of Box<T> fat pointer uses

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,10 +1,23 @@
-// use crate::List::{Cons, Nil};
-// enum List {
-//     Cons(i32, List),
-//     Nil,
-// }
+use crate::List::{Cons, Nil};
 
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+pub fn reference_pointers() {
+    let x = 5;
+    let y = &x;
+    let z = Box::new(x);
+    println!("x: {x}");
+    println!("x via y: {}", y);
+    println!("x via z: {}", z);
+    assert_eq!(x, *y);
+    assert_eq!(x, *z);
+}
 fn main() {
-    // let list = Cons(1, Cons(2, Cons(3, Nil)));
-    println!("Hello, world!");
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("list: {:#?}", list);
+    reference_pointers();
 }
