8016f64a69ddd4862a987aca98db634ac1df7df9
salimp2009@gmail.com <salimp2009@gmail.com>
2023-08-19 15:56:01 +0300

    added example for coersion from &Box<String> to &String and to &str

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -26,9 +26,24 @@ pub fn mybox_type() {
     println!("y: {:?}", *y);
 }
 
+pub fn print_name(name: &str) {
+    println!("My name is {}", name);
+}
+
+pub fn use_print_name() {
+    let m = Boxm::new(String::from("Salitos"));
+    // automatically to deref's from &Box<String> to &String
+    // and &String coerces to &str
+    print_name(&m);
+    // those are not needed; just to understand how it works
+    print_name(&m[..3]);
+    print_name(&(*m)[..3]);
+}
+
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("list: {:#?}", list);
     reference_pointers();
     mybox_type();
+    use_print_name();
 }
