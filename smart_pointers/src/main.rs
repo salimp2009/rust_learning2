32c0f3e7b051fb1d6ecb3c98dbe41451fb7a4f28
salimp2009@gmail.com <salimp2009@gmail.com>
2023-08-28 13:11:30 +0300

    added a basic example using Rc; reference counting shared pointer

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,11 +1,19 @@
+use std::rc::Rc;
+
 use smart_pointers::Boxm;
 
 use crate::List::{Cons, Nil};
+use crate::List2::{Cons2, Nil2};
 
 #[derive(Debug, Clone)]
 pub enum List {
     Cons(i32, Box<List>),
     Nil,
+}
+#[derive(Debug, Clone)]
+pub enum List2 {
+    Cons2(i32, Rc<List2>),
+    Nil2,
 }
 
 pub fn reference_pointers() {
@@ -34,6 +42,7 @@ pub fn use_print_name() {
     let m = Boxm::new(String::from("Salitos"));
     // automatically to deref's from &Box<String> to &String
     // and &String coerces to &str
+    println!("m (Boxm) : {:?}", *m);
     print_name(&m);
     // print_name(&*m);
     // those are not needed; just to understand how it works
@@ -51,9 +60,17 @@ pub fn drop_early() {
 
 pub fn list_sharedptr() {
     // example for the need of reference counting pointer
-    // let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
-    // let b = Cons(3, Box::new(a));
-    // let c = Cons(4, Box::new(a));
+    let a1 = Box::new(Cons(5, Box::new(Cons(10, Box::new(Nil)))));
+    let _b1 = Cons(3, Box::clone(&a1));
+    let _c1 = Cons(4, Box::clone(&a1));
+
+    let a = Rc::new(Cons2(5, Rc::new(Cons2(10, Rc::new(Nil2)))));
+    let b = Cons2(3, Rc::clone(&a));
+    let c = Cons2(4, Rc::clone(&a));
+    println!("a: {:?}", a);
+    println!("count of shared ref of a: {:?}", Rc::strong_count(&a));
+    println!("b: {:?}", b);
+    println!("c: {:?}", c);
 }
 
 fn main() {
@@ -63,4 +80,5 @@ fn main() {
     mybox_type();
     use_print_name();
     drop_early();
+    list_sharedptr();
 }
