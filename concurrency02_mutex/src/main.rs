2a7ac83413a501be9e9375eaeeca47a14b376a9e
salimp2009@gmail.com <salimp2009@gmail.com>
2023-09-13 20:35:39 +0300

    added example to share Mutex in multiple threads using Arc smart pointer

--- concurrency02_mutex/src/main.rs
+++ concurrency02_mutex/src/main.rs
@@ -1,10 +1,40 @@
-use std::sync::Mutex;
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
 
-fn main() {
+pub fn mutex_api() {
     let m = Mutex::new(5);
     {
         let mut num = m.lock().unwrap();
         *num = 10;
     }
     println!("Mutex m: {:?}", m);
+}
+
+pub fn sharing_mutex() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+    // for handle in handles {
+    //     handle.join().unwrap();
+    // }
+
+    handles.into_iter().for_each(|handle| {
+        handle.join().unwrap();
+    });
+    println!("Result of counter: {}", *counter.lock().unwrap());
+}
+
+fn main() {
+    mutex_api();
+    sharing_mutex();
 }
