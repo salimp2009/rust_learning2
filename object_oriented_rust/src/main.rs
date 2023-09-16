a14b1089016cf78fdfac147d6da809e5bfa3d823
salimp2009@gmail.com <salimp2009@gmail.com>
2023-09-16 22:46:43 +0300

    added SelectBox and impl Draw for that in main

--- object_oriented_rust/src/main.rs
+++ object_oriented_rust/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use object_oriented_rust::gui::{Button, Draw, Screen};
+
+#[derive(Debug)]
+struct SelectBox<'a> {
+    width: u32,
+    height: u32,
+    options: Vec<&'a str>,
 }
+
+impl<'a> Draw for SelectBox<'a> {
+    fn draw(&self) {
+        println!("drawing: {:?}", self);
+    }
+}
+
+fn main() {}
