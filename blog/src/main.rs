6e5fa687e27affa3e539c54ad65aa0b9044cd9a0
salimp2009@gmail.com <salimp2009@gmail.com>
2023-09-20 14:31:45 +0300

    completed the blog example as an example of objected oriented Rust using traits

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -1,3 +1,14 @@
+use blog::post::Post;
 fn main() {
-    println!("Hello, world!");
+    let mut post = Post::new();
+    println!("default post: {:?}", post.content());
+    post.add_text("I did a horrible mistake today :(");
+    assert_eq!(post.content(), "");
+    println!("draft post: {:?}", post.content());
+    post.request_review();
+    assert_eq!(post.content(), "");
+    println!("reviewed post: {:?}", post.content());
+    post.approve();
+    assert_eq!(post.content(), "I did a horrible mistake today :(");
+    println!("approved post: {:?}", post.content());
 }
--- blog/src/post.rs
+++ blog/src/post.rs
@@ -15,12 +15,17 @@ impl Post {
     }
 
     pub fn content(&self) -> &str {
-        ""
+        self.state.as_ref().unwrap().content(self)
     }
 
     pub fn request_review(&mut self) {
         if let Some(s) = self.state.take() {
             self.state = Some(s.request_review());
+        }
+    }
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve());
         }
     }
 }
@@ -33,12 +38,20 @@ impl Default for Post {
 
 trait State {
     fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
 }
 
 struct Draft {}
 
 impl State for Draft {
     fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
         self
     }
 }
@@ -48,5 +61,25 @@ struct PendingReview {}
 impl State for PendingReview {
     fn request_review(self: Box<Self>) -> Box<dyn State> {
         self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
     }
 }
