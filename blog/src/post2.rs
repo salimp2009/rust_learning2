b114d1956f8525fbaa42cccc10f6932120e47bd4
salimp2009@gmail.com <salimp2009@gmail.com>
2023-09-21 15:18:09 +0300

    continue with the alternative pattern to handles post states without state pattern using types

--- blog/src/post2.rs
+++ blog/src/post2.rs
@@ -2,3 +2,26 @@
 pub struct Post2 {
     content: String,
 }
+
+#[derive(Debug, PartialEq)]
+pub struct DraftPost2 {
+    content: String,
+}
+
+impl Post2 {
+    pub fn new() -> DraftPost2 {
+        DraftPost2 {
+            content: String::new(),
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+}
+
+impl DraftPost2 {
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+}
