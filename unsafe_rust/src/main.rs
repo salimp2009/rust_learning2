8da1dd626652b94caad248401e5f552c7f5e69ec
salimp2009@gmail.com <salimp2009@gmail.com>
2023-09-29 00:28:45 +0300

    added basic unsafe blocks when doiing unsafe Rust; raw pointer dereferencing

--- unsafe_rust/src/main.rs
+++ unsafe_rust/src/main.rs
@@ -1,10 +1,30 @@
-pub fn arbitrary_memory() {
+use std::{slice, usize};
+
+///  # Safety
+///  raw pointer does not change the value of original reference
+pub unsafe fn arbitrary_memory() {
     let address = 0x14;
     let r = address as *const i32;
 
     println!(" address = {:?}", address);
     println!(" pointer to address r : {:?}", r);
 }
+
+pub fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    if mid >= values.len() {
+        return (values, &mut []);
+    }
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+    // unsafe { (&mut (*ptr)[..mid], &mut (*ptr)[mid..]) }
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
 fn main() {
     let mut num = 5;
     let r1 = &num as *const i32;
@@ -26,5 +46,5 @@ fn main() {
         println!("r1: {:?}", *r1);
         println!("r2: {:?}", *r2);
     }
-    // arbitrary
+    // unsafe { arbitrary_memory() };
 }
