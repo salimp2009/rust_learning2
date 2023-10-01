88fbf0983ad85c93e6fc28bd0ebffd1f829948fd
salimp2009@gmail.com <salimp2009@gmail.com>
2023-10-02 00:27:06 +0300

    added supertraits similar to inheritance in C++ to use supertrait's functionality e.g.Display trait

--- traits_advanced/src/main.rs
+++ traits_advanced/src/main.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use std::{mem::size_of_val, ops::Add};
 use traits_advanced::point::Point;
 
@@ -48,6 +49,40 @@ impl Human {
         "Human".to_string()
     }
 }
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("{}", "*".repeat(len + 2));
+        println!("*{}*", " ".repeat(len + 4));
+    }
+}
+
+struct Point4 {
+    x: i32,
+    y: i32,
+}
+
+impl fmt::Display for Point4 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl OutlinePrint for Point4 {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
 fn main() {
     assert_eq!(
         Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
@@ -62,4 +97,5 @@ fn main() {
     println!("{}", Human::name());
     println!("{}", <Human as Pilot>::name());
     println!("{}", <Human as Wizard>::name());
+    Point4 { x: 4, y: 5 }.outline_print();
 }
