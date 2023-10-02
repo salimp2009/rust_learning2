8fabb559341151ab188e81ab7fb2fbb98965fac9
salimp2009@gmail.com <salimp2009@gmail.com>
2023-10-02 23:32:13 +0300

    basic example of using newtype pattern

--- traits_advanced/src/main.rs
+++ traits_advanced/src/main.rs
@@ -1,5 +1,9 @@
 use core::fmt;
-use std::{mem::size_of_val, ops::Add};
+use std::{
+    fmt::Display,
+    mem::size_of_val,
+    ops::{Add, Deref, DerefMut},
+};
 use traits_advanced::point::Point;
 
 struct Millimeters(u32);
@@ -83,6 +87,30 @@ impl OutlinePrint for Point4 {
         println!("{}", "*".repeat(len + 4));
     }
 }
+
+#[derive(Debug)]
+struct VecString(Vec<String>);
+
+impl Display for VecString {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0.join(", "))
+    }
+}
+impl Deref for VecString {
+    type Target = Vec<String>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+    // add code here
+}
+
+impl DerefMut for VecString {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
 fn main() {
     assert_eq!(
         Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
@@ -98,4 +126,7 @@ fn main() {
     println!("{}", <Human as Pilot>::name());
     println!("{}", <Human as Wizard>::name());
     Point4 { x: 4, y: 5 }.outline_print();
+    let w = VecString(vec!["yoow".to_string(), "cool".to_string()]);
+    println!("VecString w= {}", w);
+    w.iter().for_each(|elem| println!("elem: {}", elem));
 }
