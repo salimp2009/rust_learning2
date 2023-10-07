f568aa03f1e5d67d9762511c322871576f116683
salimp2009@gmail.com <salimp2009@gmail.com>
2023-10-07 23:50:34 +0300

    basic TCP connection using sockets, ports

--- webserver_single_thread/src/main.rs
+++ webserver_single_thread/src/main.rs
@@ -1,9 +1,24 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect::<Vec<_>>();
+    println!("Request: {:#?}", http_request);
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        println!("Connection Established");
+        let stream = stream?;
+        println!("Connection Established at: {:?}", stream);
+        handle_connection(stream);
     }
+    Ok(())
 }
