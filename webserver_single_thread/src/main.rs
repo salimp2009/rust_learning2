bc36750e3677dd31dc56ae0263aa1114d1a8f358
salimp2009@gmail.com <salimp2009@gmail.com>
2023-12-17 21:31:22 +0300

    refactored webserver_single_thread, handle_connection & removed for loop in main
    - for loop in main is refactored to iterator & for_each loop also used ? branch operator
    to unwrap or error
    - handle_connection return type is changed to std::io::Result<()>

--- webserver_single_thread/src/main.rs
+++ webserver_single_thread/src/main.rs
@@ -3,7 +3,7 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     let buf_reader = BufReader::new(&mut stream);
     let http_request = buf_reader
         .lines()
@@ -13,16 +13,23 @@ fn handle_connection(mut stream: TcpStream) {
     println!("Request: {:#?}", http_request);
 
     let response = "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes())?;
     println!("Response: {:?}", response);
+    Ok(())
 }
 
 fn main() -> std::io::Result<()> {
+    let _myvec: Vec<u8> = Vec::new();
     let listener = TcpListener::bind("127.0.0.1:7878")?;
-    for stream in listener.incoming() {
-        let stream = stream?;
-        println!("Connection Established at: {:?}", stream);
-        handle_connection(stream);
-    }
+
+    listener.incoming().for_each(|stream| {
+        println!("connecting at: {:#?}", stream);
+        handle_connection(stream.unwrap()).unwrap()
+    });
+    // for stream in listener.incoming() {
+    //     let stream = stream?;
+    //     println!("Connection Established at: {:?}", stream);
+    //     handle_connection(stream);
+    // }
     Ok(())
 }
