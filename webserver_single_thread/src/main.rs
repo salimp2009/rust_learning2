232d5ae3d7b997efe9f2df3f4d3b95b99c7a7134
salimp2009@gmail.com <salimp2009@gmail.com>
2023-12-19 21:45:16 +0300

    refactored the handle_connection to also respond to an unknown request
    - added 404.html
    - used a match case to handle the 2 cases
    - will refactor to return a tupple of status line and filename
    - will try to use iterator if possible

--- webserver_single_thread/src/main.rs
+++ webserver_single_thread/src/main.rs
@@ -6,20 +6,43 @@ use std::{
 
 fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect::<Vec<_>>();
-    println!("Request: {:#?}", http_request);
+    // let request_line = buf_reader.lines().next().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let content = fs::read_to_string("hello.html")?;
-    let length = content.len();
+    match buf_reader.lines().next().unwrap() {
+        Ok(request) if request == "GET / HTTP/1.1" => {
+            let status_line = "HTTP/1.1 200 OK";
+            let contents = fs::read_to_string("hello.html").unwrap();
+            let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
+            let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes())?;
+            stream.write_all(response.as_bytes())?;
+        }
+        _ => {
+            let status_line = "HTTP/1.1 404 NOT FOUND";
+            let contents = fs::read_to_string("404.html").unwrap();
+            let length = contents.len();
+
+            let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+            stream.write_all(response.as_bytes())?;
+        }
+    }
+
+    // let http_request = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect::<Vec<_>>();
+    // println!("Request: {:#?}", http_request);
+
+    // let status_line = "HTTP/1.1 200 OK";
+    // let content = fs::read_to_string("hello.html")?;
+    // let length = content.len();
+
+    // let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
+
+    // stream.write_all(response.as_bytes())?;
     // println!("Response: {:?}", response);
     Ok(())
 }
