ac89c18e8ca412a1f7dfb543d45675d7e8c885ea
salimp2009@gmail.com <salimp2009@gmail.com>
2023-12-28 22:02:27 +0300

    refactored handle_connection to use an array as buffer and read from stream to that buffer
    - not if this is more efficient ?

--- webserver_single_thread/src/main.rs
+++ webserver_single_thread/src/main.rs
@@ -1,6 +1,11 @@
+// clippy gives read_exact method for stream.read
+// but it causes problems and does not work since
+// it fills all the buffer
+#![allow(clippy::unused_io_amount)]
+
 use std::{
     fs,
-    io::{BufRead, BufReader, Write},
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
     thread,
     time::Duration,
@@ -9,26 +14,42 @@ use std::{
 use webserver_single_thread::thread_pool::ThreadPool;
 
 fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap()?;
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer)?;
 
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => {
-            println!("thread id:{:#?}", thread::current().id());
-            ("HTTP/1.1 200 OK", "hello.html")
-        }
-        "GET /sleep HTTP/1.1" => {
-            println!("thread id sleep: {:#?}", thread::current().id());
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "hello.html")
-        }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
+
+    // let buf_reader = BufReader::new(&mut stream);
+    // let request_line = buf_reader.lines().next().unwrap()?;
+
+    // let (status_line, filename) = match &request_line[..] {
+    //     "GET / HTTP/1.1" => {
+    //         println!("thread id:{:#?}", thread::current().id());
+    //         ("HTTP/1.1 200 OK", "hello.html")
+    //     }
+    //     "GET /sleep HTTP/1.1" => {
+    //         println!("thread id sleep: {:#?}", thread::current().id());
+    //         thread::sleep(Duration::from_secs(5));
+    //         ("HTTP/1.1 200 OK", "hello.html")
+    //     }
+    //     _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    // };
 
     let contents = fs::read_to_string(filename)?;
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes())?;
+    stream.flush().unwrap();
     println!("Response: {:?}", response);
 
     // let http_request = buf_reader
