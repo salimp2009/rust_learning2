fd8176c155dc7f266f3759e9deb8e7b8489bc054
salimp2009@gmail.com <salimp2009@gmail.com>
2024-01-01 22:52:53 +0300

    implement Joinm to simulate Join on 2 Futures
    - next will be AndThen impl for SimpleFuture so we can chain multiple Futures

--- asyncable/src/simplefuture.rs
+++ asyncable/src/simplefuture.rs
@@ -8,12 +8,10 @@ pub enum Pollm<T> {
     Pending,
 }
 
-#[allow(dead_code)]
 struct Socket {
     data: Vec<u8>,
 }
 
-#[allow(dead_code)]
 impl Socket {
     fn has_data_to_read(&self) -> bool {
         !self.data.is_empty()
@@ -27,7 +25,18 @@ impl Socket {
     }
 }
 
-#[allow(dead_code)]
+impl AsRef<Socket> for Socket {
+    fn as_ref(&self) -> &Socket {
+        self
+    }
+}
+
+impl AsMut<Socket> for Socket {
+    fn as_mut(&mut self) -> &mut Socket {
+        self
+    }
+}
+
 pub struct SocketRead<'a> {
     socket: &'a Socket,
 }
@@ -36,24 +45,56 @@ impl SimpleFuture for SocketRead<'_> {
     type Output = Vec<u8>;
 
     fn poll(&mut self, wake: fn()) -> Pollm<Self::Output> {
-        if self.socket.has_data_to_read() {
-            Pollm::Ready(self.socket.read_buf())
-        } else {
-            self.socket.set_readable_callback(wake);
-            Pollm::Pending
+        match self.socket.has_data_to_read() {
+            true => Pollm::Ready(self.socket.read_buf()),
+            _ => {
+                self.socket.set_readable_callback(wake);
+                Pollm::Pending
+            }
         }
+        // if self.socket.has_data_to_read() {
+        //     Pollm::Ready(self.socket.read_buf())
+        // } else {
+        //     self.socket.set_readable_callback(wake);
+        //     Pollm::Pending
+        // }
     }
 }
 
-#[allow(dead_code)]
+// This shows how multiple futures can be run simultaneously
+// without needing separate allocations, allowing
+// for more efficient asynchronous programs.
 pub struct Joinm<FutureA, FutureB> {
     a: Option<FutureA>,
     b: Option<FutureB>,
 }
-impl<FutureA, FutureB> SimpleFuture for Joinm<FutureA, FutureB> {
+impl<FutureA, FutureB> SimpleFuture for Joinm<FutureA, FutureB>
+where
+    FutureA: SimpleFuture<Output = ()>,
+    FutureB: SimpleFuture<Output = ()>,
+{
     type Output = ();
 
     fn poll(&mut self, wake: fn()) -> Pollm<Self::Output> {
-        todo!()
+        // check first Future is ready then empty Optional
+        if let Some(a) = &mut self.a {
+            if let Pollm::Ready(()) = a.poll(wake) {
+                self.a.take();
+            }
+        }
+
+        // check second Future is ready then empty Optional
+        if let Some(b) = &mut self.b {
+            if let Pollm::Ready(()) = b.poll(wake) {
+                self.b.take();
+            }
+        }
+
+        // check if both Optionals empty, Futures are Ready
+        // otherwise Pending
+        match self.a.is_none() && self.b.is_none() {
+            true => Pollm::Ready(()),
+            _ => Pollm::Pending,
+        }
     }
 }
