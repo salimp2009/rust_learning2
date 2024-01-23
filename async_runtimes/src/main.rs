9c3b28d72737b910c27eee9a33a46568cc116550
salimp2009@gmail.com <salimp2009@gmail.com>
2024-01-23 22:05:31 +0300

    added examples of how tokio handles concurrency & parallelism
    - tokio use tokoio::spawn to handle concurrency however there is no explicit
    control how the threads are used

--- async_runtimes/src/main.rs
+++ async_runtimes/src/main.rs
@@ -1,4 +1,5 @@
 use std::time;
+use tokio::runtime::Handle;
 use tokio::task::{JoinError, JoinHandle};
 
 pub fn tokio_builder() {
@@ -20,24 +21,64 @@ pub fn not_async_function() -> JoinHandle<()> {
     })
 }
 
-#[tokio::main]
+pub fn not_async_function2(handle: Handle) {
+    handle.block_on(async {
+        println!("second print statement");
+    })
+}
+async fn sleep_1s_blocking(task: &str) {
+    use tokio::time::{sleep, Duration};
+    println!("Entering sleep_1s_blocking({task})");
+    sleep(Duration::from_secs(1)).await; // #1
+    println!("Returning from sleep_1s_blocking({task})");
+}
+
+fn returns_option() -> Result<i32, ()> {
+    Ok(5)
+}
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 1)]
 async fn main() -> Result<(), JoinError> {
+    println!("Test 1: Run 2 async task sequentially");
+    sleep_1s_blocking("Task 1").await;
+    sleep_1s_blocking("Task 2").await;
+
+    println!("Test 2: Run 2 async task concurrently (same thread)");
+    tokio::join!(sleep_1s_blocking("Task 3"), sleep_1s_blocking("Task 4"));
+
+    println!("Test 3: Run 2 async tasks in parallel");
+    let (_, _) = tokio::join!(
+        tokio::spawn(sleep_1s_blocking("Task 5")),
+        tokio::spawn(sleep_1s_blocking("Task 6"))
+    );
+
+    let Ok(res) = returns_option() else {
+        unreachable!()
+    };
+    println!("value : {}", res);
+
     // let duration = time::Duration::from_secs(1);
     // tokio::time::sleep(duration).await;
     // // tokio_builder();
 
     // this prints first since it is awaited
-    async {
-        println!("This line prints first");
-    }
-    .await;
+    // async {
+    //     println!("This line prints first");
+    // }
+    // .await;
 
-    // this may or may not run since there is no await
-    tokio::task::spawn(async {
-        println!("This line prints sometimes, depending on how quick it runs")
-    });
+    // let handle = Handle::current();
+
+    // std::thread::spawn(move || {
+    //     not_async_function2(handle);
+    // });
+
+    // // this may or may not run since there is no await
+    // tokio::task::spawn(async {
+    //     println!("This line prints sometimes, depending on how quick it runs")
+    // });
 
     // not_async_function().await?;
-    println!("tokio is running from main runtime");
+    // println!("tokio is running from main runtime");
     Ok(())
 }
