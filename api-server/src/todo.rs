d1424dbefce2bd99b9d55aae10ef9cfd94da71a1
salimp2009@gmail.com <salimp2009@gmail.com>
2024-02-03 21:24:12 +0300

    added 2 more function for Todos struct; update & delete
    - added CreateTodo struct which has body:String and use serde
    Deserialize macro. Also added body() function to return body
    - added UpdateTodo struct which has body and id field  & uses Serde
    Deserialize macro. Added body(), completed() function to return
    fields body & id
    - need to understand the reason for this extra typesince Todos struct
    has already those fields

--- api-server/src/todo.rs
+++ api-server/src/todo.rs
@@ -1,8 +1,8 @@
+use crate::error::Error;
+
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 use sqlx::{query, query_as, SqlitePool};
-
-use crate::error::Error;
 
 #[derive(Serialize, Clone, sqlx::FromRow)]
 pub struct Todo {
@@ -29,5 +29,65 @@ impl Todo {
             .map_err(Into::into)
     }
 
+    pub async fn create(dbpool: SqlitePool, new_todo: CreateTodo) -> Result<Todo, Error> {
+        query_as("insert into todos (body) values (?) returning *")
+            .bind(new_todo.body())
+            .fetch_one(&dbpool)
+            .await
+            .map_err(Into::into)
+    }
+
+    pub async fn update(
+        dbpool: SqlitePool,
+        id: i64,
+        updated_todo: UpdateTodo,
+    ) -> Result<Todo, Error> {
+        query_as(
+            "update todos set body = ?, completed = ?, \
+            updated_at = datetime('now') where id = ? returning *",
+        )
+        .bind(updated_todo.body())
+        .bind(updated_todo.completed())
+        .bind(id)
+        .fetch_one(&dbpool)
+        .await
+        .map_err(Into::into)
+    }
+
+    pub async fn delete(dbpool: SqlitePool, id: i64) -> Result<(), Error> {
+        query("delete from todos where id = ?")
+            .bind(id)
+            .execute(&dbpool)
+            .await?;
+        Ok(())
+    }
+}
+
+#[derive(Deserialize)]
+pub struct UpdateTodo {
+    body: String,
+    completed: bool,
+}
+
+impl UpdateTodo {
+    pub fn body(&self) -> &str {
+        self.body.as_ref()
+    }
+
+    pub fn completed(&self) -> bool {
+        self.completed
+    }
+    // add code here
+}
+
+#[derive(Deserialize)]
+pub struct CreateTodo {
+    body: String,
+}
+
+impl CreateTodo {
+    pub fn body(&self) -> &str {
+        self.body.as_ref()
+    }
     // add code here
 }
