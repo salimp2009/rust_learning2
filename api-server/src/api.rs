dc95a00fc3c5e504d548becd8197d68a84568f5e
salimp2009@gmail.com <salimp2009@gmail.com>
2024-02-05 21:38:07 +0300

    completed all the functions in api.rs which will be use router.rs to create Routes

--- api-server/src/api.rs
+++ api-server/src/api.rs
@@ -1,6 +1,6 @@
 use axum::extract::{Path, State};
 use axum::Json;
-use sqlx::{Acquire, SqlitePool};
+use sqlx::SqlitePool;
 
 use crate::error::Error;
 use crate::todo::{CreateTodo, Todo, UpdateTodo};
@@ -13,4 +13,37 @@ pub(crate) async fn ping(State(dbpool): State<SqlitePool>) -> Result<String, Err
         .await
         .map(|_| "ok".to_string())
         .map_err(Into::into)
+}
+
+pub(crate) async fn todo_list(State(dbpool): State<SqlitePool>) -> Result<Json<Vec<Todo>>, Error> {
+    Todo::list(dbpool).await.map(Json::from)
+}
+
+pub(crate) async fn todo_read(
+    State(dbpool): State<SqlitePool>,
+    Path(id): Path<i64>,
+) -> Result<Json<Todo>, Error> {
+    Todo::read(dbpool, id).await.map(Json::from)
+}
+
+pub(crate) async fn todo_create(
+    State(dbpool): State<SqlitePool>,
+    Path(new_todo): Path<CreateTodo>,
+) -> Result<Json<Todo>, Error> {
+    Todo::create(dbpool, new_todo).await.map(Json::from)
+}
+
+pub(crate) async fn todo_update(
+    State(dbpool): State<SqlitePool>,
+    Path(id): Path<i64>,
+    Path(updated_todo): Path<UpdateTodo>,
+) -> Result<Json<Todo>, Error> {
+    Todo::update(dbpool, id, updated_todo).await.map(Json::from)
+}
+
+pub(crate) async fn todo_delete(
+    State(dbpool): State<SqlitePool>,
+    Path(id): Path<i64>,
+) -> Result<(), Error> {
+    Todo::delete(dbpool, id).await
 }
