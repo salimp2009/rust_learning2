cb97bc10781b3cd69a8674248fed3543d5c95d98
salimp2009@gmail.com <salimp2009@gmail.com>
2024-02-06 21:41:30 +0300

    completed create_router() function in router.rs
    - we pass database connection wrapper in Pool which wraps it using Arc
    - create new routes for alive, ready , v1/todos, v1/todos/:id with different
    methods and pass the database connection to handler as state
    - a CORS layer is added to apply CORS header
    - added HTTP tracing layer from tower_http to get request traces

--- api-server/src/router.rs
+++ api-server/src/router.rs
@@ -1,7 +1,28 @@
+use crate::api::{ping, todo_create, todo_delete, todo_list, todo_read, todo_update};
+use axum::{routing::get, Router};
+use tower_http::cors::{Any, CorsLayer};
+use tower_http::trace::TraceLayer;
+
+// database is passed to Router; which takes the ownership
 pub async fn create_router(dbpool: sqlx::Pool<sqlx::Sqlite>) -> axum::Router {
-    use crate::api::ping;
-    use axum::{routing::get, Router};
-    use tower_http::cors::{Any, CorsLayer};
-    use tower_http::trace::TraceLayer;
-    todo!()
+    Router::new()
+        // returns 200
+        .route("/alive", get(|| async { "ok" }))
+        .route("/ready", get(ping))
+        // Api routes are nested under v1
+        .nest(
+            "v1", //
+            Router::new()
+                // path /v1/todos; two methods are allowed; GET & POST
+                .route("/todos", get(todo_list).post(todo_create))
+                // path /v1/todos/:id;
+                .route(
+                    "/todos/:id",
+                    get(todo_read).put(todo_update).delete(todo_delete),
+                ),
+        )
+        // handing the database connection to Router to be passed to handlers
+        .with_state(dbpool)
+        .layer(CorsLayer::new().allow_methods(Any).allow_origin(Any))
+        .layer(TraceLayer::new_for_http())
 }
