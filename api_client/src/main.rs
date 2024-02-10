1ea8b0147a66f744f96507ece14b30cb17c2ca29
salimp2009@gmail.com <salimp2009@gmail.com>
2024-02-10 20:37:21 +0300

    started adding Commands and using Uri to use GET, POST, DELETE requests
    - need to fallback hyper older version since the newer version does not
    have all the features; will try to upgrade once finished; looks like
    some features are seperate hyper crate like hyper-util ..etc :)

--- api_client/src/main.rs
+++ api_client/src/main.rs
@@ -1,5 +1,8 @@
-use clap::Parser;
-use hyper::Uri;
+use clap::{Parser, Subcommand};
+use colored_json::prelude::*;
+use hyper::{body::HttpBody as _, header::CONTENT_TYPE, Body, Client, Method, Request, Uri};
+use serde_json::json;
+use yansi::Paint;
 
 use crate::cli::{Cli, Commands};
 
@@ -18,5 +21,66 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         uri_builder = uri_builder.authority(authority.clone());
     }
 
+    match cli.command {
+        Commands::List => {
+            request(
+                uri_builder.path_and_query("/v1/todos").build()?,
+                Method::GET,
+                None,
+            )
+            .await
+        }
+        Commands::Delete { id } => {
+            request(
+                uri_builder
+                    .path_and_query(format!("/v1/todos/{}", id))
+                    .build()?,
+                Method::DELETE,
+                None,
+            )
+            .await
+        }
+        Commands::Read { id } => {
+            request(
+                uri_builder
+                    .path_and_query(format!("/v1/todos/{}", id))
+                    .build()?,
+                Method::GET,
+                None,
+            )
+            .await
+        }
+        Commands::Create { body } => {
+            request(
+                uri_builder.path_and_query("/v1/todos").build()?,
+                Method::POST,
+                Some(json!({"body":body}).to_string()),
+            )
+            .await
+        }
+        Commands::Update {
+            id,
+            body,
+            completed,
+        } => {
+            request(
+                uri_builder
+                    .path_and_query(format!("/v1/todos/{}", id))
+                    .build()?,
+                Method::PUT,
+                Some(json!({"body":body,"completed":completed}).to_string()),
+            )
+            .await
+        }
+    }
+
+    // Ok(())
+}
+
+async fn request(
+    url: hyper::Uri,
+    method: Method,
+    body: Option<String>,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     Ok(())
 }
