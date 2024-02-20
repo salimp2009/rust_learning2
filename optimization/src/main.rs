581f6290399a8ab698549801878c2d20507b8885
salimp2009@gmail.com <salimp2009@gmail.com>
2024-02-20 20:47:28 +0300

    added an example for for loop, iter() & into_iter()
    - results are very close for 10,000,000 i32 values
    -into_iter is supposed to be slowest but in release mode it is faster
    it is probably cheaper to copy integers

--- optimization/src/main.rs
+++ optimization/src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 pub fn is_string(val: &dyn std::any::Any) {
     if val.is::<String>() {
         println!("val is String");
@@ -5,8 +7,7 @@ pub fn is_string(val: &dyn std::any::Any) {
         println!("val is not String");
     }
 }
-
-pub fn main() {
+pub fn vector_alloc_size() {
     let mut empty_vec = Vec::<i32>::new();
     (0..10).for_each(|val| {
         println!(
@@ -18,7 +19,48 @@ pub fn main() {
     });
     empty_vec.shrink_to_fit();
     println!("empty_vec cap after shrink {}", empty_vec.capacity());
-    let myval = "10".to_owned();
-    is_string(&myval);
-    println!("{}", myval);
+}
+
+pub fn big_vec_test() {
+    let big_vec = vec![0; 10_000_000];
+    let now = Instant::now();
+    for i in big_vec {
+        if i < 0 {
+            println!("this never prints");
+        }
+    }
+    println!("First loop with for took: {}", now.elapsed().as_secs_f32());
+
+    let big_vec = vec![0; 10_000_000];
+    let now = Instant::now();
+    big_vec.iter().for_each(|i| {
+        if *i < 0 {
+            println!("this never prints from iter");
+        }
+    });
+    println!(
+        "Second loop with iter took: {}",
+        now.elapsed().as_secs_f32()
+    );
+
+    let big_vec = vec![0; 10_000_000];
+    let now = Instant::now();
+    big_vec.into_iter().for_each(|i| {
+        if i < 0 {
+            println!("this never prints from iter");
+        }
+    });
+    println!(
+        "Third loop with into_iter took: {}",
+        now.elapsed().as_secs_f32()
+    );
+}
+
+pub fn main() {
+    // vector_alloc_size();
+
+    // let myval = "10".to_owned();
+    // is_string(&myval);
+    // println!("myval; is_string ? {}", myval);
+    big_vec_test();
 }
