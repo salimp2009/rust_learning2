8dd9a2bcbed84a303453ae76b98eb73933199791
salimp2009@gmail.com <salimp2009@gmail.com>
2024-02-25 21:33:37 +0300

    added 2 examples to sum squares for 1000 integers
    - parallel iterators are way slower than regular iterators
    probably need a bigger count value or more expensive functions/types
    which will be tested with regex and string

--- parallel_iter/src/main.rs
+++ parallel_iter/src/main.rs
@@ -1,6 +1,9 @@
 use ::std::time::Instant;
+use std::{char, i64};
 
 use rand::Rng;
+use rayon::prelude::*;
+use regex::Regex;
 
 pub fn generate_random_values(count: usize) -> Vec<i64> {
     let mut rng = rand::thread_rng();
@@ -10,6 +13,47 @@ pub fn generate_random_values(count: usize) -> Vec<i64> {
     ret
 }
 
+pub fn generate_random_strings(count: usize) -> Vec<String> {
+    let mut rng = rand::thread_rng();
+    let mut ret = Vec::with_capacity(count);
+
+    (0..50000).for_each(|_| {
+        let mut rstring = String::new();
+        rstring.push(char::from_u32(rng.gen_range(0..127)).unwrap_or(' '));
+        ret.push(rstring);
+        // ret.push(
+        //     char::from_u32(rng.gen_range(0..127))
+        //         .unwrap_or(' ')
+        //         .to_string(),
+        // );
+    });
+    ret
+}
+
 fn main() {
-    println!("Hello, world!");
+    let data = generate_random_values(1_000);
+
+    let start = Instant::now();
+    let sum = data
+        .iter()
+        .map(|num| num.wrapping_mul(*num))
+        .reduce(|a: i64, b: i64| a.wrapping_add(b));
+    let finish = Instant::now() - start;
+    println!(
+        "Summing squares without Rayon took: {}s, sum: {}",
+        finish.as_secs_f64(),
+        sum.unwrap()
+    );
+    // using parallel iterators
+    let start = Instant::now();
+    let sum = data
+        .par_iter()
+        .map(|num| num.wrapping_mul(*num))
+        .reduce(|| 0, |a: i64, b: i64| a.wrapping_add(b));
+    let finish = Instant::now() - start;
+    println!(
+        "Summing squares with Rayon took: {}s, sum: {}",
+        finish.as_secs_f64(),
+        sum
+    );
 }
