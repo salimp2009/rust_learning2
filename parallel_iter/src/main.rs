a2dcc038d051691ecbe703d5e8472e2057eb4bc8
salimp2009@gmail.com <salimp2009@gmail.com>
2024-02-26 21:21:40 +0300

    cleaned main; move the regex and parallel data tests into seperate functions

--- parallel_iter/src/main.rs
+++ parallel_iter/src/main.rs
@@ -19,7 +19,7 @@ pub fn generate_random_strings(count: usize) -> Vec<String> {
 
     (0..count).for_each(|_| {
         let mut rstring = String::new();
-        rstring.push(char::from_u32(rng.gen_range(0..127)).unwrap());
+        rstring.push(char::from_u32(rng.gen_range(0..127)).unwrap_or('c'));
         ret.push(rstring);
         // ret.push(
         //     char::from_u32(rng.gen_range(0..127))
@@ -30,33 +30,35 @@ pub fn generate_random_strings(count: usize) -> Vec<String> {
     ret
 }
 
-fn main() {
-    // let data = generate_random_values(1_000);
+pub fn parallel_reduce_test() {
+    let data = generate_random_values(1_000);
 
-    // let start = Instant::now();
-    // let sum = data
-    //     .iter()
-    //     .map(|num| num.wrapping_mul(*num))
-    //     .reduce(|a: i64, b: i64| a.wrapping_add(b));
-    // let finish = Instant::now() - start;
-    // println!(
-    //     "Summing squares without Rayon took: {}s, sum: {}",
-    //     finish.as_secs_f64(),
-    //     sum.unwrap()
-    // );
-    // // using parallel iterators
-    // let start = Instant::now();
-    // let sum = data
-    //     .par_iter()
-    //     .map(|num| num.wrapping_mul(*num))
-    //     .reduce(|| 0, |a: i64, b: i64| a.wrapping_add(b));
-    // let finish = Instant::now() - start;
-    // println!(
-    //     "Summing squares with Rayon took: {}s, sum: {}",
-    //     finish.as_secs_f64(),
-    //     sum
-    // );
+    let start = Instant::now();
+    let sum = data
+        .iter()
+        .map(|num| num.wrapping_mul(*num))
+        .reduce(|a: i64, b: i64| a.wrapping_add(b));
+    let finish = Instant::now() - start;
+    println!(
+        "Summing squares without Rayon took: {}s, sum: {}",
+        finish.as_secs_f64(),
+        sum.unwrap()
+    );
+    // using parallel iterators
+    let start = Instant::now();
+    let sum = data
+        .par_iter()
+        .map(|num| num.wrapping_mul(*num))
+        .reduce(|| 0, |a: i64, b: i64| a.wrapping_add(b));
+    let finish = Instant::now() - start;
+    println!(
+        "Summing squares with Rayon took: {}s, sum: {}",
+        finish.as_secs_f64(),
+        sum
+    );
+}
 
+pub fn parallel_regex_test() {
     let data = generate_random_strings(500);
 
     // Regex is Send + Sync. Can be used in parallel filter with Rayon
@@ -78,7 +80,13 @@ fn main() {
     let finish = Instant::now() - start;
     println!("Regex with Rayon took {}s", finish.as_secs_f64());
     println!("Regex with Rayon matches {:#?}s", matches);
+}
 
+fn main() {
+    // parallel_reduce_test();
+    parallel_regex_test();
+
+    // just example from Rayon
     let re = Regex::new(r"\b\w{13}\b").unwrap();
     let hay = b"I categorically deny having triskaidekaphobia.";
     assert!(re.is_match(hay));
