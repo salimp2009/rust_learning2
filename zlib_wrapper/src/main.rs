401a51a30a487ff398c733bca127f37b1fb7243c
salimp2009@gmail.com <salimp2009@gmail.com>
2024-02-28 16:14:23 +0300

    added zlib_uncompress wrapper to use C library with Rust using FFI

--- zlib_wrapper/src/main.rs
+++ zlib_wrapper/src/main.rs
@@ -33,14 +33,36 @@ pub fn zlib_compress(source: &[u8]) -> Vec<u8> {
         dest
     }
 }
+pub fn zlib_uncompress(source: &[u8], max_dest_len: usize) -> Vec<u8> {
+    unsafe {
+        let source_len = source.len() as c_ulong;
+        let mut dest_len = max_dest_len as c_ulong;
+        let mut dest: Vec<u8> = Vec::with_capacity(max_dest_len);
+        uncompress(
+            dest.as_mut_ptr(),
+            &mut dest_len,
+            source.as_ptr(),
+            source_len,
+        );
+        dest.set_len(dest_len as usize);
+        dest
+    }
+}
 
 fn main() {
     let hello_world = "hello_world".as_bytes();
     let hello_world_compressed = zlib_compress(hello_world);
     println!(
-        "hello_world_compressed: {:#?}, compressed len: {}, hello_world len:{}",
+        "hello_world_compressed: {:#?},\ncompressed len: {}, hello_world len:{}",
         hello_world_compressed,
         hello_world_compressed.len(),
         hello_world.len()
+    );
+
+    let hello_world_uncompressed = zlib_uncompress(&hello_world_compressed, 100);
+    assert_eq!(hello_world, hello_world_uncompressed);
+    println!(
+        "hello_world_uncompressed : {}",
+        String::from_utf8(hello_world_uncompressed).expect("Invalid character")
     );
 }
