27b8ac8072c8c3289e27ad6d2ac14af334288b16
salimp2009@gmail.com <salimp2009@gmail.com>
2024-03-31 22:22:16 +0300

    added lazy_static to test global values in tests
    - global/shared state cannot be used safely; using lazy_static solves
    that problem

--- correctness_tests/src/lib.rs
+++ correctness_tests/src/lib.rs
@@ -8,19 +8,43 @@ pub fn add<T: WrappingAdd<Output = T>>(left: T, right: T) -> T {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use lazy_static::lazy_static;
     use proptest::prelude::*;
-    use std::sync::atomic::{AtomicI32, Ordering};
-
+    use std::sync::{
+        atomic::{AtomicI32, Ordering},
+        Arc,
+    };
     static mut COUNT: AtomicI32 = AtomicI32::new(0);
+    // below does not compile since static variable can only be called in
+    // const functions, tuple structs and tuple variants
+    // static COUNT2: Arc<AtomicI32> = Arc::new(AtomicI32::new(0));
 
+    lazy_static! {
+        static ref COUNT2: Arc<AtomicI32> = Arc::new(AtomicI32::new(0));
+    }
     // these count test may fail depending on the order
     // if assert_eq! are commented out;
     // since cargo test runs on multiple threads
+
+    #[test]
+    fn test_lazy_static() {
+        let count = Arc::clone(&COUNT2);
+        count.fetch_add(1, Ordering::AcqRel);
+        println!("COUNT2: first test {:?}", count);
+    }
+
+    #[test]
+    fn test_lazy_static2() {
+        let count = Arc::clone(&COUNT2);
+        count.fetch_add(1, Ordering::AcqRel);
+        println!("COUNT2: second test {:?}", count);
+    }
+
     #[test]
     fn test_count_unsafe() {
         unsafe {
             COUNT.fetch_add(1, Ordering::AcqRel);
-            println!("COUNT1: {:#?}", COUNT);
+            println!("COUNT1: test1 {:?}", COUNT);
             // assert_eq!(COUNT.load(Ordering::Acquire), 1);
         }
     }
@@ -28,7 +52,7 @@ mod tests {
     fn test_count_unsafe2() {
         unsafe {
             COUNT.fetch_add(1, Ordering::AcqRel);
-            println!("COUNT2: {:#?}", COUNT);
+            println!("COUNT1: test2 {:?}", COUNT);
             // assert_eq!(COUNT.load(Ordering::Acquire), 2);
         }
     }
